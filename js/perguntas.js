3222d3be2f4ef5cb78ae4913da895f631bd0ef71
gabriel1876 <108006916+gabriel1876@users.noreply.github.com>
2024-10-02 15:37:41 -0300

    12

--- js/perguntas.js
+++ js/perguntas.js
@@ -3,15 +3,14 @@ export const perguntas = [
         enunciado: "Assim que você entrou no valorant pela primeira vez você jogou com um personagem que tinha habilidades para marcar inimigos, qual era este personagem?",
         alternativas: [
             {
-                texto: "Não me lembro do nome mas era um personagem que tinha um coruja robotica irada!",
+                texto: "SOVA um personagem muito legal que tem acessoa tecnologia avançada",
                 afirmacao: [
-                    "Não se lembrava do personagem.",
-                    "No entanto achava um personagem muito legal."
+                    "CORRETO ELE E O SOVA O PERSONAGEM QUE TEM UMA TECNOLOGIA DE SPOTS."
                 ],
                 proxima: 1,
             },
             {
-                texto: "Quero apreender a jogar com ele!",
+                texto: "Não lembro o nome do personagem :(!",
                 afirmacao: [
                     "Queria melhorar com o personagem.",
                     "Ele considerou um personagem legal e por isso quis continuar jogando com ele."
@@ -21,10 +20,10 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "Nas primeiras semana oque você achou do personagem SOVA?",
+        enunciado: "Qual seu mapa favorito?",
         alternativas: [
             {
-                texto: "Jogando muito e utilizando videos do youtube para melhorar, comecei a ter uma certa maestria com o personagem.",
+                texto: "haven.",
                 afirmacao: [
                     "Utilizava muito bem as habilidades do personagem.",
                     "Sempre marcando alguem do time inimigo.",
@@ -33,7 +32,7 @@ export const perguntas = [
                 proxima: 3,
             },
             {
-                texto: "Você esta tendo um jogo bom? exemplo um KDA bom?.",
+                texto: "ASCENT.",
                 afirmacao: [
                     "Mesmo ajudando meu time com as habilidades, mal conseguia abates ou kills.",
                     "Comecei a montar uma rotina de treino de mira para melhora-la.",
@@ -44,10 +43,10 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "Depois desses treinamentos você considera que melhorou sua gameplay de forma geral?",
+        enunciado: "Qual sua arma primaria favorita entre essas duas",
         alternativas: [
             {
-                texto: "Depois de algumas semana treinando alcancei a patente do jogo Diamante.",
+                texto: "Vandal.",
                 afirmacao: [
                     "Conseguiu abates de forma mais tranquila.",
                     "Não morria com tanta facilidade.",
@@ -56,7 +55,7 @@ export const perguntas = [
                 proxima: 3,
             },
             {
-                texto: "Estou me divertindo muito mais com o jogo.",
+                texto: "Phantom.",
                 afirmacao: [
                     "Sentiu mais felicidade em jogar.",
                     "Achou que era muito mais fácil jogar o jogo."
@@ -66,10 +65,10 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "Conseguiu evoluir de patente?",
+        enunciado: "Na sua opinião qual o melhor time entre esses dois?",
         alternativas: [
             {
-                texto: "Não muito bem, esta muito dificil ja estou na patente Ascendente que e uma a frente de diamante mas estou travado nela.",
+                texto: "Loud.",
                 afirmacao: [
                     "Dificuldade em evoluir a patente oponentes que conhecem mais o jogo.",
                     "Querendo evoluir mais ainda."
@@ -77,7 +76,7 @@ export const perguntas = [
                 proxima: 5,
             },
             {
-                texto: "Comecei a jogar com outros personagens.",
+                texto: "SENTINELS.",
                 afirmacao: [
                     "O SOVA ja não era o seu personagem favorito e não estava agradando tanto seu estilo de jogo.",
                     "SE tornou um jogador flex. Joga com todos os personagens do jogo ou o mais adequado para o mapa que vai jogar."
@@ -87,10 +86,10 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "Achou um personagem que te agrada mais a jogar?",
+        enunciado: "Qual sua pistol favorita?",
         alternativas: [
             {
-                texto: "Sim, o personagem OMEN estou me divertindo muito jogando com a clase controlador.",
+                texto: "Ghost.",
                 afirmacao: [
                     "Ele considera o personagem muito bom.",
                     "Esta gostando de jogar com a nova clase de personagem Controlador."
@@ -98,7 +97,7 @@ export const perguntas = [
                 proxima: 5,
             },
             {
-                texto: "Jogando com mais cautela valorizando a vida e fazendo as smokes.",
+                texto: "Sherif.",
                 afirmacao: [
                     "Percebeu que sua vida e muito importante por isso não estava tentando trocar tiro com os inimigos.",
                     "Sempre fazendo excelentes smokes para o seu time."
@@ -108,10 +107,10 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "E agora conseguiu alcançar uma patente maior?",
+        enunciado: "MELHOR STREAMER",
         alternativas: [
             {
-                texto: "Sim alcancei a patente do Radiante.",
+                texto: "TCKUUUUUU.",
                 afirmacao: [
                     "Esta jogando muito bem.",
                     "Alcançou a maior patente do jogo."
@@ -119,7 +118,7 @@ export const perguntas = [
                 proxima: 7,
             },
             {
-                texto: "Demorei muito tempo mas estou satisfeito.",
+                texto: "OTSUKA O GORDAO DO ESMAGA.",
                 afirmacao: [
                     "Demorou em torno de 1 ano e meio de dedicação.",
                     "Satisfação em alcançar tao conquista."
@@ -129,10 +128,10 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "E agora que alcançou a sua maior meta, oq vai fazer?",
+        enunciado: "Melhor Dueslista na sua opinião?",
         alternativas: [
             {
-                texto: "Planejo ganhar dinheiro com isso pois e algo que me faz bem.",
+                texto: "JETT.",
                 afirmacao: [
                     "Lucrar com oque gosta de fazer.",
                     "isso faz bem a ele, então imagina se ele consegui ganhar dinheiro com isso, vai ser 2x felicidade."
@@ -140,7 +139,7 @@ export const perguntas = [
                 proxima: 7,
             },
             {
-                texto: "Mas não sei por onde começar lives ou campeonatos.",
+                texto: "NEON.",
                 afirmacao: [
                     "Indeciso entre jogar de forma profissional.",
                     "Indeciso em fazer as pessoas rirem com sua jogabilidade."
@@ -150,17 +149,17 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "Ao final da discussão, você escolheu oque para seguir como carreira?",
+        enunciado: "Melhor CAPITÂO DE TEAMS PROFISSIONALS",
         alternativas: [
             {
-                texto: "Fazer lives para as outras pessoas se entreterem a final, fazer outras pessoas rirem e minha especialidade.",
+                texto: "SAADHACK.",
                 afirmacao: [
                     "fazer outras pessoas rirem e se divertirem com suas lives.",
                     "Especialidade em fazer pessoas rirem."
                 ],
             },
             {
-                texto: "Espero que de certo afinal jogar e algo que eu amo e se eu conseguir fazer outras pessoas rirem com e se divertirem com isso eu vou ficar muito feliz.",
+                texto: "SACY.",
                 afirmacao: [
                     "Esperança em lucrar com o jogo",
                     "Compartilhando sua gameplay para fazer outras pessoas felizes.",
